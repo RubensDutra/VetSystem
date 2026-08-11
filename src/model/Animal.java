abd55aaf8a67424007fc3bb1eb17f19f7a8b49e8
RubensDutra <rubensdutramf@gmail.com>
2026-08-11 08:44:50 -0300

    feat: add Animal getter and setter

--- src/model/Animal.java
+++ src/model/Animal.java
@@ -11,5 +11,67 @@ public class Animal {
     private int idade;
     private int id_cliente;
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
 }
