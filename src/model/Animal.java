a138d2732bab9ebc962ed99090e5d20dc467cc54
RubensDutra <rubensdutramf@gmail.com>
2026-08-11 08:43:55 -0300

    feat: add Animal attributes

--- src/model/Animal.java
+++ src/model/Animal.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Animal {
+
+    private int id;
+    private String nome;
+    private String especie;
+    private String raca;
+    private double peso;
+    private String sexo;
+    private int idade;
+    private int id_cliente;
+
+
 }
